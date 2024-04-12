2fb9878a26ce56c30b69ad2d56a3ad9aefe9a725
202548 <202548>
2024-04-12 17:39:09 +0200

    Add form validation and register page

--- tic-tac-toe-frontend/src/app/login/login.component.ts
+++ tic-tac-toe-frontend/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { InputFieldComponent } from '../elements/input-field/input-field.component';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,50 @@ import { FormControl } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  userNameControl = new FormControl('');
-  passwordControl = new FormControl('');
+  userNameControl = new FormControl('', [Validators.required]);
+  passwordControl = new FormControl('', [Validators.required]);
+
+  userNameError?: string;
+  passwordError?: string;
+
+  // Validate a required input field and retrieve the error message if invalid
+  static validateRequired(
+    control: FormControl,
+    label: string
+  ): string | undefined {
+    return control.errors && control.errors['required']
+      ? 'The ' + label + ' is required!'
+      : undefined;
+  }
+
+  constructor(protected router: Router) {}
+
+  // Submit the login form
+  submit() {
+    this.validateUserName();
+    this.validatePassword();
+
+    this.userNameControl.markAsTouched();
+    this.passwordControl.markAsTouched();
+
+    if (!this.userNameError && !this.passwordError) {
+      this.router.navigate(['game']);
+    }
+  }
+
+  // Validate username input
+  protected validateUserName() {
+    this.userNameError = LoginComponent.validateRequired(
+      this.userNameControl,
+      'username'
+    );
+  }
+
+  // Validate password input
+  protected validatePassword() {
+    this.passwordError = LoginComponent.validateRequired(
+      this.passwordControl,
+      'password'
+    );
+  }
 }
---
+++ tic-tac-toe-frontend/src/app/register/register.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { InputFieldComponent } from '../elements/input-field/input-field.component';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  AbstractControl,
+  FormControl,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { LoginComponent } from '../login/login.component';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [InputFieldComponent, MatButtonModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent extends LoginComponent {
+  passwordConfirmationControl = new FormControl('', [
+    Validators.required,
+    this.passwordConfirmationValidator(),
+  ]);
+
+  passwordConfirmationError?: string;
+
+  // Submit the register form
+  override submit() {
+    this.validateUserName();
+    this.validatePassword();
+    this.validatePasswordConfirmation();
+
+    this.userNameControl.markAsTouched();
+    this.passwordControl.markAsTouched();
+    this.passwordConfirmationControl.markAsTouched();
+
+    if (
+      !this.userNameError &&
+      !this.passwordError &&
+      !this.passwordConfirmationError
+    ) {
+      this.router.navigate(['game']);
+    }
+  }
+
+  // Validate password confirmation input
+  private validatePasswordConfirmation() {
+    this.passwordConfirmationError = LoginComponent.validateRequired(
+      this.passwordConfirmationControl,
+      'password confirmation'
+    );
+
+    if (
+      !this.passwordConfirmationError &&
+      this.passwordConfirmationControl.errors &&
+      this.passwordConfirmationControl.errors['notIdentical']
+    )
+      this.passwordConfirmationError =
+        'Both the password and the password confirmation need to be identical!';
+  }
+
+  // Validator to check whether the password and confirmed password are identical
+  private passwordConfirmationValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      let errors = null;
+      console.log('validator', control.value);
+
+      if (this.passwordControl.value !== control.value)
+        errors = { notIdentical: true };
+      return errors;
+    };
+  }
+}
