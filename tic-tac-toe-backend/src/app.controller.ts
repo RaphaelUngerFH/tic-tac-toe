991bfd8e18ded33fa48913b202c403ffdf986183
nikolardlvc <nikola7@live.at>
2024-04-22 23:03:55 +0200

    add routes for register & get users

--- tic-tac-toe-backend/src/app.controller.ts
+++ tic-tac-toe-backend/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RegisterDTO } from './dto/register.dto';
+import { User } from './models/user.model';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/users')
+  getUsers(): User[] {
+    return this.appService.getUsers();
+  }
+
+  @Post('/register')
+  register(@Body() registerDto: RegisterDTO) {
+    return this.appService.register(registerDto);
   }
 }
--- tic-tac-toe-backend/src/app.service.ts
+++ tic-tac-toe-backend/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { RegisterDTO } from './dto/register.dto';
+import { User } from './models/user.model';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private users: User[] = [];
+
+  getUsers(): User[] {
+    return this.users;
+  }
+
+  register(registerDto: RegisterDTO) {
+    const user = new User();
+    user.username = registerDto.username;
+    user.password = registerDto.password;
+
+    this.users.push(user);
+
+    return user;
   }
 }
